daba5a12b9c0833c68a435ed10d2c5b8d5a7e2bc
Jeremiah McKeey <jmcke24@lsu.edu>
2025-09-17 22:49:28 -0500

    hw1
    Code for warmup as well as phonebook

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,47 @@
 class PhoneBook
     def initialize
+        @entries = []
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        return false unless valid_number_format?(number)
+
+        return false if person_exists?(name)
+
+        if is_listed && number_is_listed?(number)
+            return false
     end
+    @entries << {name: name, number: number, is_listed: is_listed}
+    return true
+end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        entry = @entries.find {|e| e[:name] == name && e[:is_listed]}
+        return entry ? entry[:number] : nil
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        entry = @entries.find {|e| e[:number] == number && e[:is_listed]}
+        return entry ?  entry[:name] : nil
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        names = @entries.select { |e| e[:number].start_with?(areacode + "-")}
+        .map {|e| e[:name]}
+        return names
     end
+    private 
+    
+    def valid_number_format?(number) 
+        return false unless number.match?(/^\d{3}-\d{3}-\d{4}$/)
+        return false if number.start_with?("0")
+        return true
+    end
+    def person_exists?(name)
+        @entries.any? {|e| e[:name] ==name}
+    end
+    def number_is_listed?(number)
+        @entries.any?{|e| e[:number] == number && e[:is_listed]}
 end
+end
+
--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,43 @@
 def fib(n)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    return[] if n == 0
+    result = [0]
+    return result if n == 1
+
+    result << 1 
+    (2...n).each do |i|
+        result << result[i-1] + result[i-2]
+end
+result
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    str = n.to_s
+    str == str.reverse
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    sorted = a.sort.reverse
+    sorted[n]
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    return "" if s.empty?
+    counts = Hash.new(0)
+    s.each_char { |c| counts[c] += 1}
+    counts.max_by {|_, v| v}[0]
 end
 
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    return nil if arr1.length != arr2.length
+    Hash[arr1.zip(arr2)]
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    #raise Exception, "Not Implemented"
+    hash.map{ |k, v| [k, v]}
 end
